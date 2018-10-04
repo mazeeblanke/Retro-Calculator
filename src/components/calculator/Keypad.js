db069710d43f877e4576404081714b0b0640a09f
linux08 <abimbola130@gmail.com>
2018-10-04 16:49:16 +0100

    Added square and square rrot functionality

--- src/components/calculator/Keypad.js
+++ src/components/calculator/Keypad.js
@@ -1,44 +1,44 @@
-import React, { Component } from 'react';
-import { keypadKeys } from '../../utils/constants';
-import Calculator from '../calculator'
+import React, { Component } from "react";
+import { keypadKeys } from "../../utils/constants";
+import Calculator from "../calculator";
 export default class Keypad extends Component {
-  handleClick = (key) => {
-    switch(key) {
-      case 'c':
-        this.props.clear()
-        break
-      case 'Del':
-        this.props.delete()
-        break
-      case '=':
-        this.props.evaluate()
-        break
+  handleClick = key => {
+    switch (key) {
+      case "c":
+        this.props.clear();
+        break;
+      case "Del":
+        this.props.delete();
+        break;
+      case "=":
+        this.props.evaluate();
+      case "√":
+        this.props.squareRoot();
+        break;
+      case "x^2":
+        this.props.square();
+        break;
       default:
-        this.props.calculate(key)
-        break
+        this.props.calculate(key);
+        break;
     }
- }
-  render () {
-
+  };
+  render() {
     return (
       <div className="keypad">
-        {
-          keypadKeys.map((block,index) => {
-            return (
-              <div key={index} className="block">
-                {
-                  block.map(key => (
-                    <Calculator.Button
-                      key={key}
-                      onButtonClick={this.handleClick}
-                      buttonKey={key}
-                    />
-                  ))
-                }
-              </div>
-            )
-          })
-        }
+        {keypadKeys.map((block, index) => {
+          return (
+            <div key={index} className="block">
+              {block.map(key => (
+                <Calculator.Button
+                  key={key}
+                  onButtonClick={this.handleClick}
+                  buttonKey={key}
+                />
+              ))}
+            </div>
+          );
+        })}
       </div>
     );
   }
--- src/store/reducers/calculateReducer.js
+++ src/store/reducers/calculateReducer.js
@@ -1,64 +1,75 @@
-import * as types from '../types';
-import calculate from '../../utils/calculate'
+import * as types from "../types";
+import calculate from "../../utils/calculate";
 
 let initialState = {
-  expression: '',
+  expression: "",
   total: 0
-}
+};
 
-function setExpression({ expression, total}, action) {
-
-  if (
-    /[\d]*[-+%*/.]$/.exec(expression) &&
-    /[-+%*/.]/.exec(action.payload)
-  ) {
-    console.log('b', expression);
-    expression = expression.slice(0, expression.length - 1)
-    console.log('a', expression);
+function setExpression({ expression, total }, action) {
+  if (/[\d]*[-+%*/.]$/.exec(expression) && /[-+%*/.]/.exec(action.payload)) {
+    console.log("b", expression);
+    expression = expression.slice(0, expression.length - 1);
+    console.log("a", expression);
   }
 
   switch (action.type) {
     case types.SET_EXPRESSION:
-      if (['+', '/', '*', '%'].includes(action.payload) && !expression) {
-        return `${total}${action.payload}`
+      if (["+", "/", "*", "%"].includes(action.payload) && !expression) {
+        return `${total}${action.payload}`;
       }
-      return `${!expression && total ? total : ''}${expression + action.payload}`
+      return `${!expression && total ? total : ""}${expression +
+        action.payload}`;
     default:
-      return expression
+      return expression;
   }
 }
 
 export default (state = initialState, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case types.SET_EXPRESSION:
-      let expression = setExpression(state, action)
-
+      let expression = setExpression(state, action);
       return {
         ...state,
         expression,
         total: calculate(expression) || state.total
-      }
+      };
     case types.CLEAR_EXPRESSION:
       return {
         ...state,
-        expression: '',
+        expression: "",
         total: 0
-      }
+      };
     case types.DELETE_LAST_EXPRESSION_ENTRY:
-      let exp = state.expression
-      exp = exp.split('').slice(0, exp.length - 1).join('')
+      let exp = state.expression;
+      exp = exp
+        .split("")
+        .slice(0, exp.length - 1)
+        .join("");
       return {
         ...state,
         expression: exp,
         total: calculate(exp)
-      }
+      };
     case types.EVALUATE_EXPRESSION:
       return {
         ...state,
-        expression: '',
+        expression: "",
         total: calculate(state.expression) || state.expression || state.total
-      }
+      };
+    case types.SQUARE:
+      return {
+        ...state,
+        expression: "",
+        total: state.expression * state.expression
+      };
+    case types.SQUARE_ROOT:
+      return {
+        ...state,
+        expression: "",
+        total: Math.sqrt(state.expression)
+      };
     default:
-      return state
+      return state;
   }
-}
+};
